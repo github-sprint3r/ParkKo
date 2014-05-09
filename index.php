d11f048bc7a3505752560d83c678dc160d203faa
sahaki <toners2print@gmail.com>
2014-05-09 15:19:49 +0700

    gu boy

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<title>PARK KO</title>
+</head>
+<body>
+<div class="contrainer">
+	<div class="header"><strong>PARK KO</strong></div>
+    <div class="boxBody">
+    <div class="wrapper">
+    	<div class="leftfrme">
+        <!-- Left frame -->
+        	<!-- Left Welcomeframe frame -->
+            <div class="welcomebox-top">
+            Test Zard
+            </div>
+            <div class="welcomebox-bottom">
+            </div>
+            <!-- Left Welcomeframe frame -->
+            <!-- Left Menu frame -->
+            <div class="menubox">
+            
+            </div>
+            <!-- Left Menu frame -->
+		<!-- Left frame -->
+         </div>
+         
+         <div class="rightframe">
+         	<!-- Right frame -->
+         	<div class="detailbox">
+            <?php include_once 'parkko_change.php' ?>
+            </div>
+            <!-- Right frame -->
+         </div>
+    </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ parkko_change.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<body>
+<form action="" method="post">
+	<fieldset>
+    <legend>จ่ายค่าจอดรถ</legend>
+	<table width="90%">
+    	<tbody>
+        	<tr>
+            	<td width="30%" align="right">ค่าจอดรถ : </td>
+                <td><input type="text" id="cost" name="cost" value="500" style="text-align:right; width:100px;" /> บาท</td>
+            </tr>
+            <tr>
+            	<td width="30%" align="right">จำนวนเงินที่รับ : </td>
+                <td><input type="text" id="payment" name="payment" value="" style="text-align:right; width:100px;" /> บาท</td>
+            </tr>
+            <tr>
+            	<td width="30%" align="right"></td>
+                <td><input type="submit" name="btt-01" id="btt-01" value="คำนวณ" style="width:100px;" /></td>
+            </tr>
+        </tbody>
+    </table>
+    </fieldset>
+    <br />
+    <fieldset>
+    <legend>คำนวณค่าใช้จ่าย/เงินทอน</legend>
+	<table width="90%">
+    	<tbody>
+        	<tr>
+            	<td width="30%" align="right">จำนวนเงินทอน : </td>
+                <td align="right">0.00 </td>
+                <td width="50%"> บาท</td>
+            </tr>
+            <tr>
+            	<td align="right">แบงค์ 500 : </td>
+                <td align="right">0 </td>
+                <td> ฉบับ</td>
+            </tr>
+            <tr>
+            	<td align="right">แบงค์ 100 : </td>
+                <td align="right">0 </td>
+                <td> ฉบับ</td>
+            </tr>
+            <tr>
+            	<td align="right">แบงค์ 50 : </td>
+                <td align="right">0 </td>
+                <td> ฉบับ</td>
+            </tr>
+            <tr>
+            	<td align="right">แบงค์ 20 : </td>
+                <td align="right">0 </td>
+                <td> ฉบับ</td>
+            </tr>
+            <tr>
+            	<td align="right">เหรียญ 10 : </td>
+                <td align="right">0 </td>
+                <td> เหรียญ</td>
+            </tr>
+        </tbody>
+    </table>
+    </fieldset>
+</form>
+</body>
+</html>

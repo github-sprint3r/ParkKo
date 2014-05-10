a388c6e31dae22989d3013ebbd10c6f7fc21b997
Jullachai <onimaruz@hotmail.com>
2014-05-10 15:57:39 +0700

    -

--- login/index.php
+++ login/index.php
@@ -19,6 +19,7 @@
 			$timeUpdate = date('Y-m-d H:i:s');
 			$data = array($user_profile->identifier,$user_profile->firstName,$user_profile->gender,$user_profile->lastName,$user_profile->profileURL,$user_profile->region,$user_profile->displayName,'7',$timeUpdate,'false',1,$timeUpdate);
 			
+			$db->insert($field,$data)->save();
 			
 		}
 		

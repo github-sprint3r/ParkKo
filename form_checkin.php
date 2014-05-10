f5d3819b87fcf952d3f26e6bb70d2b416ec6aae8
kidsana <kidsana.p@gmail.com>
2014-05-10 11:06:15 +0700

    by Kidsana

---
+++ form_checkin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<body>
+<form action="" method="post">
+<table width="200" border="0" >
+  <tr>
+    <td align="right">เลขทะเบียน: </td>
+    <td><input type="text" id="car_number" name="car_number" value=""/></td>
+    <td align="right">จังหวัด: </td>
+    <td><input type="text" id="province" name="province" value=""/></td>
+    <td><input type="submit" name="bt_check_in" value="เริ่มจอด"/></td>
+  </tr>
+</table>
+</form>
+</body>
+</html>
